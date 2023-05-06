cbaf979fd39ddcecfac4c88bc138d761d63ff6f5
jahongir1808 <jahongir.developer18@gmail.com>
2023-05-06 10:50:12 +0500

    changed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 import { USER } from "../const";
 
 let parse = JSON.parse(localStorage.getItem(USER))?.role;
-let parseId = JSON.parse(localStorage.getItem(USER))._id;
+let parseId = JSON.parse(localStorage.getItem(USER))?._id;
 
 export const ROLE = parse;
 export const USER_ID = parseId;
